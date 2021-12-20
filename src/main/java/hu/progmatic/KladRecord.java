3836bc9a294923175df73460e2ec6805ee057f0b
Molnár Zsolt <molnar.zsolt.1984@gmail.com>
2021-12-20 20:23:11 +0100

    MZs

---
+++ src/main/java/hu/progmatic/KladRecord.java
@@ -0,0 +1,11 @@
+package hu.progmatic;
+
+public class KladRecord {
+  Integer szuloKategoriaID;
+  String nev;
+
+  public KladRecord(String nev, Integer szuloKategoriaID) {
+    this.szuloKategoriaID = szuloKategoriaID;
+    this.nev = nev;
+  }
+}
