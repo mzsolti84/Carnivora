e63b96495833e879b0d0ec865eb8e204c6c6eaa5
CsRZs <csakanyrobertzsombor@gmail.com>
2021-12-21 10:38:46 +0100

    Query optimalizálása (Query query = ... megszüntetése)

--- src/main/java/hu/progmatic/Carnivora.java
+++ src/main/java/hu/progmatic/Carnivora.java
@@ -7,91 +7,92 @@ import org.sql2o.Sql2o;
 import java.util.*;
 
 public class Carnivora {
-  public final Sql2o adatbazis = new Sql2o(
-      "jdbc:mysql://localhost:3306/progmatic",
-      "progmatic",
-      "progmatic"
-  );
+    public final Sql2o adatbazis = new Sql2o(
+            "jdbc:mysql://localhost:3306/progmatic",
+            "progmatic",
+            "progmatic"
+    );
 
-  public void init() {
-    try (Connection connection = adatbazis.open()) {
-      tablaTorleseHaLetezik(connection);
-      tablaLetrehozasa(connection);
+    public void init() {
+        try (Connection connection = adatbazis.open()) {
+            tablaTorleseHaLetezik(connection);
+            tablaLetrehozasa(connection);
+        }
     }
-  }
 
-  private void tablaTorleseHaLetezik(Connection connection) {
-    Query query = connection.createQuery("""
-        DROP TABLE if exists klad, klad_leiras, faj, faj_leiras;
-        """);
-    query.executeUpdate();
-  }
-
-  private void tablaLetrehozasa(Connection connection) {
-    connection.createQuery("""
-        create table klad (
-          ID int auto_increment primary key,
-          szuloKategoriaID int,
-          nev text,
-        foreign key (szuloKategoriaID) references klad(ID)
-        )
-        """).executeUpdate();
-
-    connection.createQuery("""
-        create table klad_leiras (
-          ID int auto_increment primary key,
-          nev text not null,
-          latinNev text not null,
-          leiras longtext,
-        foreign key (ID) references klad(ID)
-        )
-        """).executeUpdate();
-
-    connection.createQuery("""
-        create table faj (
-          ID int auto_increment primary key,
-          szuloKategoriaID int,
-          nev text,
-        foreign key (szuloKategoriaID) references klad(ID)
-        )
-        """).executeUpdate();
-
-    connection.createQuery("""
-        create table faj_leiras (
-          ID int auto_increment primary key,
-          nev text not null,
-          latin_nev text not null,
-          leiras longtext not null,
-          specialista int not null,
-          fotoURL text not null,
-          wikiURL text,
-        foreign key (ID) references faj(ID)
-        )
-        """).executeUpdate();
-  }
-
-  public void adatbazisToltKlad(List<KladRecord> lista) {
-    try (Connection connection = adatbazis.open()) {
-      for (KladRecord kladRecord : lista) {
+    private void tablaTorleseHaLetezik(Connection connection) {
         connection.createQuery("""
-                insert into klad (szuloKategoriaID, nev) values (:szuloKategoriaID, :nev)
-                """).addParameter("szuloKategoriaID", kladRecord.szuloKategoriaID)
-            .addParameter("nev", kladRecord.nev)
-            .executeUpdate();
-      }
+                DROP TABLE if exists klad, klad_leiras, faj, faj_leiras;
+                """).executeUpdate();
     }
-  }
 
-  public void adatbazisToltKladLeiras(List<KladLeirasRecord> lista) {
-    try (Connection connection = adatbazis.open()) {
-      for (KladLeirasRecord kladLeirasRecord : lista) {
+    private void tablaLetrehozasa(Connection connection) {
         connection.createQuery("""
-                insert into klad_leiras (nev, latinNev, leiras) values (:nev, :latinNev, :leiras)
-                """).addParameter("nev", kladLeirasRecord.nev)
-            .addParameter("latinNev", kladLeirasRecord.latinNev)
-            .addParameter("leiras", kladLeirasRecord.leiras)
-            .executeUpdate();
-      }
+                create table klad (
+                  ID int auto_increment primary key,
+                  szuloKategoriaID int,
+                  nev text,
+                foreign key (szuloKategoriaID) references klad(ID)
+                )
+                """).executeUpdate();
+
+        connection.createQuery("""
+                create table klad_leiras (
+                  ID int auto_increment primary key,
+                  nev text not null,
+                  latinNev text not null,
+                  leiras longtext,
+                foreign key (ID) references klad(ID)
+                )
+                """).executeUpdate();
+
+        connection.createQuery("""
+                create table faj (
+                  ID int auto_increment primary key,
+                  szuloKategoriaID int,
+                  nev text,
+                foreign key (szuloKategoriaID) references klad(ID)
+                )
+                """).executeUpdate();
+
+        connection.createQuery("""
+                create table faj_leiras (
+                  ID int auto_increment primary key,
+                  nev text not null,
+                  latin_nev text not null,
+                  leiras longtext not null,
+                  specialista int not null,
+                  fotoURL text not null,
+                  wikiURL text,
+                foreign key (ID) references faj(ID)
+                )
+                """).executeUpdate();
     }
-  }
+
+    public void adatbazisToltKlad(List<KladRecord> lista) {
+        try (Connection connection = adatbazis.open()) {
+            for (KladRecord kladRecord : lista) {
+                connection.createQuery("""
+                                insert into klad (szuloKategoriaID, nev) values (:szuloKategoriaID, :nev)
+                                """)
+                        .addParameter("szuloKategoriaID", kladRecord.szuloKategoriaID)
+                        .addParameter("nev", kladRecord.nev)
+                        .executeUpdate();
+            }
+        }
+    }
+
+    public void adatbazisToltKladLeiras(List<KladLeirasRecord> lista) {
+        try (Connection connection = adatbazis.open()) {
+            for (KladLeirasRecord kladLeirasRecord : lista) {
+                connection.createQuery("""
+                                insert into klad_leiras (nev, latinNev, leiras) values (:nev, :latinNev, :leiras)
+                                """)
+                        .addParameter("nev", kladLeirasRecord.nev)
+                        .addParameter("latinNev", kladLeirasRecord.latinNev)
+                        .addParameter("leiras", kladLeirasRecord.leiras)
+                        .executeUpdate();
+            }
+        }
+    }
 }
