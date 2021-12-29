8fe235e01113afd875a7570300e2b2e485f058c4
CsRZs <csakanyrobertzsombor@gmail.com>
2021-12-29 17:41:51 +0100

    Sárga háromszöges ajánlások átnézése és alkalmazása

--- src/main/java/hu/progmatic/Main.java
+++ src/main/java/hu/progmatic/Main.java
@@ -1,13 +1,5 @@
 package hu.progmatic;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Carnivora carnivora = new Carnivora();
