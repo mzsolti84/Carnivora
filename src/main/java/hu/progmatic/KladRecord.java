85591d2970c4e01dd434e2912dd362ecd1d66be7
CsRZs <csakanyrobertzsombor@gmail.com>
2021-12-30 15:20:39 +0100

    // KladRecord: / Factoryba felvettem egy Integer típusú szuloKategoriaID-t és annak null értéket adtam, hogy a visszatérő new KladRecordban ne legyen beégetve a "null" érték a szuloKategoriaID-hez.

--- src/main/java/hu/progmatic/KladRecord.java
+++ src/main/java/hu/progmatic/KladRecord.java
@@ -1,34 +1,37 @@
 package hu.progmatic;
 
 public class KladRecord {
-  Integer szuloKategoriaID;
-  String nev;
-  String latinNev;
-  String leiras;
+    Integer szuloKategoriaID;
+    String nev;
+    String latinNev;
+    String leiras;
 
-  public KladRecord(Integer szuloKategoriaID, String nev, String latinNev, String leiras) {
-    this.szuloKategoriaID = szuloKategoriaID;
-    this.nev = nev;
-    this.latinNev = latinNev;
-    this.leiras = leiras;
-  }
+    public KladRecord(Integer szuloKategoriaID, String nev, String latinNev, String leiras) {
+        this.szuloKategoriaID = szuloKategoriaID;
+        this.nev = nev;
+        this.latinNev = latinNev;
+        this.leiras = leiras;
+    }
 
-  public static KladRecord factory (String line) {
-    String[] lineTomb = line.split(",");
-    String nev = lineTomb[0];
-    String latinNev = lineTomb[1];
-    String leiras = null;
-    if (lineTomb.length == 3) { leiras = lineTomb[2]; }
-    return new KladRecord(null, nev, latinNev, leiras);
-  }
+    public static KladRecord factory(String line) {
+        String[] lineTomb = line.split(",");
+        String nev = lineTomb[0];
+        String latinNev = lineTomb[1];
+        String leiras = null;
+        Integer szuloKategoriaID = null;
+        if (lineTomb.length == 3) {
+            leiras = lineTomb[2];
+        }
+        return new KladRecord(szuloKategoriaID, nev, latinNev, leiras);
+    }
 
-  @Override
-  public String toString() {
-    return "KladRecord{" +
-        "szuloKategoriaID=" + szuloKategoriaID +
-        ", nev='" + nev + '\'' +
-        ", latinNev='" + latinNev + '\'' +
-        ", leiras='" + leiras + '\'' +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "KladRecord{" +
+                "szuloKategoriaID=" + szuloKategoriaID +
+                ", nev='" + nev + '\'' +
+                ", latinNev='" + latinNev + '\'' +
+                ", leiras='" + leiras + '\'' +
+                '}';
+    }
 }
