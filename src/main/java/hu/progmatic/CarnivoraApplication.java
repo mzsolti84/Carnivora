751c602f0276e06bfca9b72638376850a0ea8691
Sárközy Ádám <sarkozy.adam@magnetbank.hu>
2022-01-31 11:40:44 +0100

    Init

---
+++ src/main/java/hu/progmatic/CarnivoraApplication.java
@@ -0,0 +1,13 @@
+package hu.progmatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnivoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarnivoraApplication.class, args);
+    }
+
+}
--- src/main/java/hu/progmatic/carnivoraProject/CarnivoraController.java
+++ src/main/java/hu/progmatic/carnivoraProject/CarnivoraController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -38,7 +35,7 @@ public class CarnivoraController {
 
     // GET MAPPINGEK --------------------------------------------------------------------------------
 
-    @GetMapping("/carnivoraProject/carnivora")
+    @RequestMapping("/")
     public String species() {
         return carnivora();
     }
