f8c0e1393eed92e8671478fca49ef85274f91168
mzsolti84 <molnar.zsolt.1984@gmail.com>
2022-02-03 09:17:39 +0100

    Projekt

--- src/test/java/hu.progmatic/carnivoraProject/FajlTest.java
+++ src/test/java/hu.progmatic/carnivoraProject/FajlTest.java
@@ -1,6 +1,6 @@
 package hu.progmatic.carnivoraProject;
 
-import hu.progmatic.klad.TesztFileTeljesNev;
+import hu.progmatic.carnivoraProject.user.TesztFileTeljesNev;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
