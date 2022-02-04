dc25f5fee720ab9c165c16f298934af7a7c964fd
CsRZs <csakanyrobertzsombor@gmail.com>
2022-02-04 18:36:40 +0100

    Kis rendrakás

--- src/main/java/hu/progmatic/carnivora/FajDto.java
+++ src/main/java/hu/progmatic/carnivora/FajDto.java
@@ -6,4 +6,16 @@ import lombok.Data;
 @Data
 @Builder
 public class FajDto {
+    private Integer id;
+    private Integer szuloId;
+    private String szuloNev;
+    private String leiras;
+    private String nev;
+    private String latinNev;
+    private VeszelyeztetettKategoriak veszelyeztetettBesorolas;
+    private Tureshatar turesHatar;
+    private String fotoURL;
+    private String wikiURL;
+
+
 }
